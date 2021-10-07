4441cb068431af02d57225fc67373e7ce5d45d2b
Sven Jostkleigrewe <sven@jostkleigrewe.com>
2021-10-07 17:50:17 +0200

    first commit

---
+++ src/Service/IntentCoreService.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace Jostkleigrewe\IntentCoreBundle\Service;
+
+/**
+ * Class IntentCoreService
+ *
+ * @package   Jostkleigrewe\IntentCoreBundle\Service
+ * @author    Sven Jostkleigrewe <sven@jostkleigrewe.com>
+ * @copyright 2019 Sven Jostkleigrewe
+ */
+class IntentCoreService
+{
+
+    /**
+     * AlexaCoreService constructor.
+     *
+     */
+    public function __construct(
+    ) {
+    }
+
+}
